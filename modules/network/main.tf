c3989dc9eb1e7e4fe95a276f8e192780b543dc4e
Ubuntu <ubuntu@ip-172-31-22-143.ec2.internal>
2025-08-31 07:40:12 +0000

    added eip

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -101,3 +101,35 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.env}-eip-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-private-rt"
+  }
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
